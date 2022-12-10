c640d1d340239228888fe3f6520dae4781f8cc84
SHARGINJAY PATEL <sharginjaypatel12021999@gmail.com>
2022-12-11 00:51:37 +0530

    polymophism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
+    public  static class A{
+        public String meth(){return "Invoking method from class A";}
+    }
+    public static class B extends A{
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+
+    public static void main(String[]args)
+    {
+        A a=new B();
+        a.meth();
+    }
 
 }
