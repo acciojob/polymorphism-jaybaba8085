f81af87078b9bede777e9b66cea6ec3b3bcf9751
SHARGINJAY PATEL <sharginjaypatel12021999@gmail.com>
2022-12-11 17:35:50 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,32 @@
 package com.driver;
 
-public class Main {
-    public  static class A{
-        public String meth(){return "Invoking method from class A";}
-    }
-    public static class B extends A{
-        public String meth() {
-            return "Method is overridden in Extendend class B";
+import java.util.*;
+import java.lang.*;
+
+public class Main
+{
+    public static class P{
+        int a;
+        int b;
+        int c;
+        int product(int a,int b){
+            int c = a*b;
+            return c;
+        }
+        int product(int a,int b,int c){
+            int d = a*b*c;
+            return d;
+        }
+        double product(double a,double b){
+            double ans = a*b;
+            return ans;
         }
     }
-
-
-    public static void main(String[]args)
-    {
-        A a=new B();
-        a.meth();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        P p = new P();
+        System.out.println(p.product(1,2));
+        System.out.println(p.product(2,3,4));
+        System.out.println(p.product(1.2,3.4));
     }
-
 }
